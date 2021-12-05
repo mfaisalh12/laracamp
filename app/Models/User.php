6ad6e886d130b063848a983c5ba5c8b712891c66
Faisal <mfaisalhidayatullah@gmail.com>
2021-12-05 22:46:29 +0700

    update checkouts table

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,9 @@ class User extends Authenticatable
         'email_verified_at',
         'password',
         'avatar',
+        'occupation',
+        'phone', 
+        'address',
         'is_admin'
     ];
 
